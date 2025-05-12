1a0aa9660b964c9caf9db54c5ac47b51557cced4
kidozh <11661760+kidozh@users.noreply.github.com>
2025-05-12 19:29:44 +0100

    add updates

--- compare-fp8-list.py
+++ compare-fp8-list.py
@@ -3,16 +3,19 @@ import sys
 
 import numpy as np
 import torch
-device = "mps"
+device = "cpu"
+
+bit_depth = [64, 32, 16]
 
 if __name__ in "__main__":
     for product_size in [64, 128, 256, 512, 1024]:
-        for np_type in [np.float64, np.float32, np.float16]:
+        for type_idx, np_type in enumerate([np.float64, np.float32, np.float16]):
             signal_arr = np.random.random((product_size, 1)).astype(np_type)
-            print(np_type, product_size, sys.getsizeof(signal_arr))
+            print(bit_depth[type_idx], product_size, sys.getsizeof(signal_arr))
 
-        for torch_type in [torch.float8_e5m2, torch.float8_e4m3fn]:
+        for torch_type in [torch.float8_e5m2]:
             signal_arr = torch.randn(product_size, 1, device=device, dtype=torch.float16)
-            signal_arr_lower_bit = signal_arr.to(torch_type)
-            print(torch_type, product_size, sys.getsizeof(signal_arr_lower_bit))
+            signal_arr_lower_bit = signal_arr.to(torch_type).cpu()
+            signal_size = signal_arr_lower_bit.element_size() * signal_arr_lower_bit.nelement()
+            print(8, product_size, signal_size)
 
